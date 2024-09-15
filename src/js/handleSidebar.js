ff285028f00a29a8f3ad414db60e852119c357cb
Mike <pacstrap@mx.idalon.org>
2024-09-15 14:44:01 +1000

    Add sidebar seperator and indicator for collapsed.

--- src/js/handleSidebar.js
+++ src/js/handleSidebar.js
@@ -1,8 +1,11 @@
 document.getElementById('buildingLinkButton').addEventListener('click', () => {
     const buildingLinksGroup = document.getElementById('buildingLinksGroup');
+    const buildingToggenIndic = document.getElementById('buildingLinkButtonIndic');
     if (buildingLinksGroup.classList.contains('hidden')) {
         buildingLinksGroup.classList.remove('hidden');
+        buildingToggenIndic.classList.remove('hidden');
     } else {
         buildingLinksGroup.classList.add('hidden');
+        buildingToggenIndic.classList.add('hidden');
     }
 });
